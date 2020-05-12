262732b61ac0d7dd7d91454e65e064d56837d542
Matejop <matej.fibiger@gmail.com>
2020-05-12 20:44:38 +0200

    Vymázání blbostí

--- src/cz/maze/main/MazeMain.java
+++ src/cz/maze/main/MazeMain.java
@@ -11,20 +11,11 @@ public class MazeMain {
         f.setSize(498,605);
         f.setExtendedState(Frame.MAXIMIZED_BOTH);
         f.setLayout(null);//no layout manager
-
-
         ClosingWindowListenerAdapter w = new ClosingWindowListenerAdapter();
-
-        // set reaction to closing window
-        // add and create class that will handle window close event
         f.addWindowListener(w);
-
-        // Define canvas class with paint method overrided
-        // will be called when window is drawn
         BarrierCanvas myCanvas = new BarrierCanvas();
         
         f.setVisible(true);
-        
         myCanvas.setLocation(f.getInsets().left,f.getInsets().top);
         myCanvas.setSize(f.getWidth()-f.getInsets().left - f.getInsets().right, f.getHeight()-f.getInsets().top - f.getInsets().bottom);      
         f.add(myCanvas);
